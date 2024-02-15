d103b3144cdaecf1a046110dbe53809fb5a8c581
jagdish <abc.gmail.com>
2024-02-15 12:09:29 +0530

    modified from eclipse push & pull

--- jprojectforGit/src/jpackageforGit/JClassforGit.java
+++ jprojectforGit/src/jpackageforGit/JClassforGit.java
@@ -5,6 +5,7 @@ public class JClassforGit {
 	public static void main(String[] args) {
 		System.out.println("Hello we are testing for Git");
 		System.out.println("Hello we are writing from git");
+		System.out.println("Hellow We are Modified from Eclipse");
 
 	
 	}
