071f559faf53b68ba24e6b88003379e9e9dcb653
jpempiric <157690161+jpempiric@users.noreply.github.com>
2024-02-15 11:46:58 +0530

    Update JClassforGit.java

--- jprojectforGit/src/jpackageforGit/JClassforGit.java
+++ jprojectforGit/src/jpackageforGit/JClassforGit.java
@@ -4,7 +4,9 @@ public class JClassforGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello we are testing for Git");
+		System.out.println("Hello we are writing from git");
 
+	
 	}
 
 }
